10e4e0d147a7959725305f54b444f9a7919033d6
Marko Sanković <sankovic.marko@gmail.com>
2018-02-14 11:06:56 +0100

    Remove S3 item and export directory after both ova files are deployed

--- exports.sh
+++ exports.sh
@@ -6,10 +6,8 @@ deploy () {
     cp export-i-base.ovf exports/$1/$2.ovf
     sed -i -e "s/export-i-base/${2}/g" exports/$1/$2.ovf
     openssl sha1 exports/$1/$2-disk-1.vmdk exports/$1/$2.ovf > exports/$1/$2.mf
-    tar -cf exports/$1/$2.ova -C exports/$1/ $2.ovf $2-disk-1.vmdk $2.mf
+    tar -cvf exports/$1/$2.ova -C exports/$1/ $2.ovf $2-disk-1.vmdk $2.mf
     aws s3 cp exports/$1/$2.ova s3://oblac-drives-vms
-    aws s3 rm s3://oblac-drives-vms/$1.ova
-    rm -rf exports/$1
 }
 
 mkdir -p exports
@@ -23,9 +21,11 @@ do
             continue
         fi
         mkdir exports/$name
-        tar -xf $file -C exports/$name/
+        tar -xvf $file -C exports/$name/
         deploy $name oblac-drives
         suffix=`date +%Y%m%d-%H%M`
         deploy $name oblac-drives-${suffix}
+        aws s3 rm s3://oblac-drives-vms/$name.ova
+        rm -rf exports/$name
     fi
 done
