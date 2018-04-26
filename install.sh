70f79e20743c391c383b174c92b65c5842cad683
Nikola Zivkovic <zivke85@gmail.com>
2018-04-26 15:40:53 +0200

    Add the installation script for the native installation

---
+++ install.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+if test "$#" -ne 2; then
+    echo 'Usage: "./install.sh 18.2 eth0" (18.2 - OBLAC Drives bundle version; eth0 - EtherCAT network interface)'
+    exit 1
+fi
+
+if ! bash -c 'command -v ansible >/dev/null 2>&1'; then
+    sudo apt-get update && \
+    sudo apt-get install -y software-properties-common && \
+    sudo apt-add-repository -y ppa:ansible/ansible && \
+    sudo apt-get update && \
+    sudo apt-get install -y ansible
+fi
+
+ansible-playbook -i "localhost," -c local native.yml --extra-vars "bundle_version=${1} ethercat_interface=${2}"
