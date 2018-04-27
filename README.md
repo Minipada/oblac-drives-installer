6edca7b2d6da60f89021d0fe9155d0ec11c52138
Nikola Zivkovic <zivke85@gmail.com>
2018-04-27 11:13:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+oblac-drives-native
---
+++ install.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if test "$#" -ne 2; then
+    echo 'Usage: "./install.sh 18.2 eth0" (18.2 - OBLAC Drives bundle version; eth0 - EtherCAT network interface)'
+    exit 1
+fi
+
+if ! bash -c 'command -v ansible >/dev/null 2>&1'; then
+    sudo apt-get update && \
+    sudo apt-get install -y software-properties-common && \
+    sudo apt-add-repository -y ppa:ansible/ansible && \
+    sudo apt-get update && \
+    sudo apt-get install -y ansible
+fi
+
+# A workaround for the jmespath problem - must be installed before starting Ansible
+if ! bash -c 'dpkg -l python-jmespath >/dev/null 2>&1'; then
+    sudo apt-get install -y python-jmespath
+fi
+
+ansible-playbook -i "localhost," -c local native.yml --extra-vars "bundle_version=${1} ethercat_interface=${2}"
