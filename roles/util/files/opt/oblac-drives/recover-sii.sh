13382e439ce8c596cb08343e0007bd6da04cf9e0
Nikola Zivkovic <zivke85@gmail.com>
2018-04-27 17:00:31 +0200

    Make sure the SII file path is always correct

--- roles/util/files/opt/oblac-drives/recover-sii.sh
+++ roles/util/files/opt/oblac-drives/recover-sii.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+WORK_DIR=$(dirname "$0")
+
 echo "Recovering the SII..."
 sudo ethercat reg_write -p 0 --type int8 0x500 0
-sudo ethercat sii_write -p 0 /home/ubuntu/scripts/somanet_cia402.sii
+sudo ethercat sii_write -p 0 ${WORK_DIR}/somanet_cia402.sii
 echo "PLEASE POWER CYCLE THE DRIVE!"
