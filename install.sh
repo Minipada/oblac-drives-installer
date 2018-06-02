fa973a9992c5e3ad26784fc1b7dabcbe797fad95
Nikola Zivkovic <zivke85@gmail.com>
2018-06-02 22:20:51 +0200

    Stop the installation if Ansible cannot be installed

--- install.sh
+++ install.sh
@@ -10,6 +10,12 @@ if ! bash -c 'command -v ansible >/dev/null 2>&1'; then
     sudo apt-add-repository -y ppa:ansible/ansible && \
     sudo apt-get update && \
     sudo apt-get install -y ansible
+
+    rc=$?;
+    if [[ $rc != 0 ]]; then
+        echo 'Failed to install Ansible. Please run "sudo apt-get update" and check for any errors. Once the errors are resolved, try installing again.';
+        exit $rc;
+    fi
 fi
 
 # A workaround for the jmespath problem - must be installed before starting Ansible
