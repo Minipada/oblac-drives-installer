ceb0182529d66904ff109aa80eddc101e29b13f0
Nikola Zivkovic <zivke85@gmail.com>
2018-07-11 13:42:40 +0200

    Fix: Configure the EtherCAT master before setting up its services

--- roles/util/files/opt/oblac-drives/ethercat-update-conf.sh
+++ roles/util/files/opt/oblac-drives/ethercat-update-conf.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-MASTER0_DEVICE="$(cat /sys/class/net/ens33/address)"
+# Default interface used inside the OBLAC Drives VM
+INTERFACE="ens33"
+
+# If available, use the interface provided as the script argument
+if [[ $# -eq 1 ]]; then
+    INTERFACE=${1}
+fi
+
+MASTER0_DEVICE="$(cat /sys/class/net/${INTERFACE}/address)"
 cp /opt/etherlab/etc/sysconfig/ethercat /etc/ethercat.conf
 sed -i "s/DEVICE_MODULES=\"\"/DEVICE_MODULES=\"generic\"/g" /etc/ethercat.conf
 sed -i "s/MASTER0_DEVICE=\"\"/MASTER0_DEVICE=\"${MASTER0_DEVICE}\"/g" /etc/ethercat.conf
