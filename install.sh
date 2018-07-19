9f79cfb8b66301bca2070be77d95fb80975e341e
Marko Sanković <sankovic.marko@gmail.com>
2018-07-19 15:03:14 +0200

    Introduce targets: native, vm and up2

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ echo
 source scripts/install-ansible.sh
 
 ansible-playbook -i "localhost," -c local native.yml && \
-echo "Installation was successful. Open \"localhost:6789\" in your browser."
+echo "Installation was successful."
