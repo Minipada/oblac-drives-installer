f5ebe6046f7dc1145d1f95b187144d553a6b2c2a
Marko Sanković <sankovic.marko@gmail.com>
2018-07-21 22:13:38 +0200

    Use default port of 80 to bind oblac-drives container to in up2

--- up2.sh
+++ up2.sh
@@ -8,5 +8,5 @@ echo
 
 source scripts/install-ansible.sh
 
-ansible-playbook -i "localhost," -c local up2.yml && \
+ansible-playbook -i "localhost," -c local up2.yml --extra-vars="oblac_drives_port=80" && \
 echo "Installation was successful."
